c9be412984f5300d8eb4009f148a94440cafdbdd
eduardo-072 <eduardosilvaaa072@gmail.com>
2023-10-06 20:48:48 -0300

    ta dificiu faze isso :(

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿const string resposta_sim = "SIM";
+const string resposta_nao = "NÃO";
+
+string EstaSeMovendo = "";
+string deveria = "";
+
+Console.WriteLine("ESTÁ SE MEXENDO?");
+EstaSeMovendo = Console.ReadLine();
+
+if (EstaSeMovendo == resposta_sim){
+   Console.WriteLine("DEVERIA?");
+   deveria = Console.ReadLine();
+
+if (deveria == resposta_sim){
+    Console.WriteLine("OK");
+
+}else{
+    deveria = Console.ReadLine()!.ToUpper();
+    Console.WriteLine("USE SILVER TAPE");
+}
+}else{
+    Console.WriteLine("DEVERIA?");
+    deveria = Console.ReadLine()!.ToUpper();
+
+if (deveria == resposta_sim){
+    Console.WriteLine("OK");
+
+}else{
+    deveria = Console.ReadLine()!.ToUpper();
+    Console.WriteLine("USE SILVER TAPE");
+
+}
+}
